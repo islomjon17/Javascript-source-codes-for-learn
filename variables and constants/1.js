2254836147dea5cd90405c15e1c8efb6412006f3
islomjon17 <turgunovislomjon107@gmail.com>
2024-02-28 16:45:34 +0500

    updated

---
+++ variables and constants/1.js
@@ -0,0 +1,64 @@
+// let o'zgaruvchini elon qilish uchun ishlatiladi
+// let name = 'Salom dunyo';
+
+
+// console.log(name);
+
+// 1. kalit so'z bo'lmasligi kerak.
+// if else for 
+
+// 2 o'zgruvchi nomi raqam bilanboshlanishi mumkin emas
+
+// 1name, 17ddd
+
+// 3 o'zgaruvchilar nomida probel yoki chiqicha bo'lishi mumkin emas
+
+
+// 4 Katta va kichik harflarda farq qiladi...
+
+
+// 5 o'zgaruvchi nomi mazmunli bo'ishi kerak.  
+// x, ab, gh, jk, pooo 
+
+
+// 6 bir paytni o'zida kopplab o'zgaruvchi qabul qilish mumkin.
+
+// let firstname = "Islomjon";
+
+// let age = 19;
+
+// let gender = "M"
+
+/* 
+contant o'zgarmaslarni elon qilish
+*/
+
+// const h = 6.626;
+
+// console.log(h);
+
+
+/// O'ZGARUVCHILARNING TURLAR 
+/* 
+##############################
+##############################
+######1. PRIMITIVE\VALUE #####
+##############################
+##############################
+######2. REFRANCE ############
+##############################
+##############################
+##############################
+PRIMITIVE\VALUE   ############################## STRING/NUMBER/BOOLEN/UNDEFINED/NULL/SYMBOL
+##############################
+*/
+
+
+let name = "Islomjon"; //STRING
+let age = 19; //NUMBER
+let RealMadrid = true; //BOOLEN
+
+let Mbappe = undefined; ///UNDEFINED
+
+let choosecar = null; // null
+choosecar = "Merc";
---
+++ variables and constants/Objectlar/1.js
@@ -0,0 +1,18 @@
+// obyekt hosil qilish kalit qiymatlar
+
+let person = {
+    name:  "shokirr",
+    age:  14
+}
+
+let car = {
+    name:  "shokirr",
+    age:  14
+}
+
+car.name = "Nissan";
+car["age"] = 17;
+
+
+console.log(person)
+console.log(car['age'])
