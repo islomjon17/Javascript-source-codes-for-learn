0c57a3b84c8997a14079e254ba6e534ed8623eb6
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-05 17:19:14 +0500

    updated

---
+++ function(funksiyalar)/1.js
@@ -0,0 +1,23 @@
+/// functions
+
+// function greet(name) { // name : parametr, value: argument
+//     console.log("Hello world, " + name);
+// }
+
+// greet("Salom dunyo")
+
+// function pc(ram, rom, gpu, cpu, monitor){
+//     console.log("Ram, " + ram + " Rom, " + rom + " gpu, ", + gpu + " cpu, ", + cpu + " Monitor, " + monitor)
+// }
+
+
+// pc(16, 256, "RTX 4060", "Ryzen 7 6800h", "Full hd ips");
+
+function sqr(num) {
+    return num * num;
+
+}
+
+let result = sqr(55);
+console.log(sqr(55));
+console.log(result);
