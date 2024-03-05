f35726ed69ac9f4cccee02004566968769492b57
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-05 16:17:24 +0500

    updated

--- Objectlar/1.js
+++ Objectlar/1.js
@@ -1,18 +1,41 @@
 // obyekt hosil qilish kalit qiymatlar
 
-let person = {
-    name:  "shokirr",
-    age:  14
+// let person = {
+//     name:  "shokirr",
+//     age:  14
+// }
+
+// let car = {
+//     name:  "shokirr",
+//     age:  14
+// }
+
+// car.name = "Nissan";
+// car["age"] = 17;
+
+
+// console.log(person)
+// console.log(car['age'])
+
+
+spremastered  = {
+    Title : "Marvel's Spider-Man Remastered minimum system requirements",
+    OS :"Windows 10 64-bit.",
+    Processor : "Intel Core i3-4160, 3.6 GHz or AMD equivalent. ",
+    Memory : " 8 GB RAM.",
+    Graphics : " NVIDIA GTX 950 or AMD Radeon RX 470.",
+    DirectX : " Version 12.",
+    Storage:  "75 GB available space "
 }
 
-let car = {
-    name:  "shokirr",
-    age:  14
-}
+console.log(spremastered);
 
-car.name = "Nissan";
-car["age"] = 17;
+spremastered.Title = "Marvel's Spider-Man Remastered recomendet system requirements";
+spremastered.OS = "Windows 10-11 64-bit";
+spremastered.Processor = "Intel Core i9-14600kh, 3.6 GHz or AMD equivalent.";
+spremastered.Memory = "16 GB RAM";
+spremastered.Graphics = "NVIDIA RTX 1950 or AMD Radeon RX 470";
+spremastered.DirectX = "Version 12";
+spremastered.Storage = "100  GB available space ";
 
-
-console.log(person)
-console.log(car['age'])
+console.log(spremastered);
