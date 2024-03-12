8c465e8e8e46dacb3079297b074fc7404037a29f
islomjon17 <turgunovislomjon107@gmail.com>
2024-03-12 20:12:43 +0500

    updated

---
+++ arifmetik(operators(operatorlar))/1.js
@@ -0,0 +1,36 @@
+/* 
+let a = 5;
+let b = 3;
+let sum = a + b; // Qoshish
+let difference = a - b; // Ayirsh
+let product = a * b; // Ko'paytirish
+let quotient = a / b; // Bolish
+let remainder = a % b; // Modul (bo'limning qolgan qismi)
+let increment = a++; // Qo'shuvchi
+let decrement = b--; // Ayruvchi
+
+
+*/
+
+let x =7;
+let y = 11;
+
+console.log(x+y); // 18
+console.log(x-y); // -4
+console.log(x*y); // 77
+console.log(x/y); // 0.63
+console.log(x%y); // 7
+console.log(x**7) // 823543
+
+
+////////////////////////////////////////////////////////////////
+
+console.log(++x)// 8
+
+console.log(x++)// 8
+
+/* 
+x ++ == x = x + 1
+x += 3 === x = x + 3
+
+*/
