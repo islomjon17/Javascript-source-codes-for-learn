41be49c034e9f74c51283d1002136d663ef08e36
islomjon17 <turgunovislomjon107@gmail.com>
2024-05-07 17:40:46 +0500

    updated

--- Ternary shartli operatorlari/1.js
+++ Ternary shartli operatorlari/1.js
@@ -1,4 +1,15 @@
-let point = 55;
-let result = point > 60 ? 'Otdi' : 'Otmadi';
+// let point = 50
+// let result = point > 60 ? 'otdi' : 'otmadi'
 
-console.log(result);
+// let points = 50
+// let result2 = points > 40 ? 'otdi' : 'otmadi'
+
+
+// console.log(result)
+// console.log(result2)
+
+
+let azo = false;
+let mijoz_azo = azo === true ? '20%' : '5%';
+
+console.log(mijoz_azo);
