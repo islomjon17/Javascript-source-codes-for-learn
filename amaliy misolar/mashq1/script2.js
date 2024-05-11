8c08cc98b9a308a5aaa9066d433483a141bb0b7b
islomjon17 <turgunovislomjon107@gmail.com>
2024-05-11 10:05:07 +0500

    updated

---
+++ amaliy misolar/mashq1/script2.js
@@ -0,0 +1,19 @@
+// let color1 = "oq";
+// let color2 = "qora";
+
+// let color3 = color2
+// color2 = color1
+// color1 = color3
+
+// console.log(color1);
+// console.log(color2)
+
+let color1 = "oq";
+let color2 = "qora";
+
+let color3 = color2
+color2 = color1
+color1 = color3
+
+console.log(color1);
+console.log(color2)
--- arifmetik(operators(operatorlar))/1.js
+++ arifmetik(operators(operatorlar))/1.js
@@ -12,50 +12,5 @@ let decrement = b--; // Ayruvchi
 
 */
 
-let x =7;
-let y = 11;
-
-console.log(x+y); // 18
-console.log(x-y); // -4
-console.log(x*y); // 77
-console.log(x/y); // 0.63
-console.log(x%y); // 7
-console.log(x**7) // 823543
-/*
-
-Operator 	Example 	Same As
-= 	        x = y 	    x = y
-+= 	        x += y 	    x = x + y
--= 	        x -= y 	    x = x - y
-*= 	        x *= y 	    x = x * y
-/= 	        x /= y 	    x = x / y
-%= 	        x %= y 	    x = x % y
-**= 	    x **= y 	x = x ** y
-*/
-
-////////////////////////////////////////////////////////////////
-
-console.log(++x)// 8
-
-console.log(x++)// 8
-
-/* 
-x ++ == x = x + 1
-x += 3 === x = x + 3
-
-*/
 
 
-
-// String operatorlari
-
-
-let text1 = "Islomjon";
-let text2 = "Turg'unov";
-let text3 = text1 + " " + text2;
-
-console.log(text3)
-
-let text_1 = "What a very ";
-text_1 += "nice day"; 
-console.log(text_1)
