9941a4da8d6b03760205cc8b1046050812b436f8
islomjon17 <turgunovislomjon107@gmail.com>
2024-09-20 17:19:56 +0500

    updated

---
+++ amaliy misolar/codial/lessons/lesson3/main.js
@@ -0,0 +1,80 @@
+// // let word = "  He llo"
+// // word.concat("", "world")
+// // console.log(word);
+// // word.includes("r")
+// // console.log(word);
+// // word.replace("ll", "drow")
+// // console.log(word);
+// // word.slice(2,3)
+// // console.log(word);
+// // word.split("")
+// // console.log(word);
+// // word.toLowerCase()
+// // console.log(word);
+// // word.toUpperCase()
+// // console.log(word);
+// // word.trim()
+// // console.log(word);
+// 1)
+// let Name_my = "islomjon"
+// console.log(Name_my.lenght());
+
+// 2 Foydalanuvchi kiritgan  stringning 7- indeksdagi harfi aniqlang.
+// foydalanuvhi ketma ket kiritgan Ikki stringni
+//  birlashtirib yangi string yarating
+
+// let text = "HELLO WORLD";
+// let char = text.charCodeAt(7);
+// console.log(char);
+// 3
+// let text = "HELLO WORLD";
+// let char = text.toUpperCase(7);
+// console.log(char);
+// 4
+// let text = "HELLO WORLD";
+// let char = text.toLowerCase(7);
+// console.log(char);
+
+// 5
+// let text = "HELLO WORLD";
+// let char = text.slice(0,5);
+// console.log(char);
+// 6
+// let Name_my = "islombek"
+// console.log(Name_my.replace("bek", jon))
+
+// 7
+// let text1 = "      Hello World!      ";
+// console.log(text1.trim());
+
+// 8
+// const greeting = "Assalomu alaykum";
+// const index = 5;
+
+// if (index >= 0 && index < greeting.length) {
+//     alert("Indeksdagi belgi: " + greeting[index]);
+// } else {
+//     alert("Noto'g'ri indeks!");
+// }
+
+// 9
+
+// let text = "Assalomu alaykum";
+// console.log(text.indexOf("a"));
+
+// 10
+// let text1 = "Lorem ipsum dolor sit amet."
+// let text2 = "adipisicing elit. Molestiae, cupiditate."
+// let text3 = text1.concat(text2)
+// console.log(text3.toLowerCase);
+
+// 11
+
+// let ism = "Islomjon";
+// let familya = "Turg'unov";
+// let full_name = ism.concat(familya);
+// if (full_name.length > 14) {
+//   console.log("Ism juda uzun");
+// } else {
+//   console.log("Normal holat");
+// }
