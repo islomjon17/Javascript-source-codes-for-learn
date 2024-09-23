4b43531711141268987b8e79afa6a714c7b78f08
islomjon17 <turgunovislomjon107@gmail.com>
2024-09-23 10:34:22 +0500

    updated

--- amaliy misolar/codial/homework1.1/homework1.3/main.js
+++ amaliy misolar/codial/homework1.1/homework1.3/main.js
@@ -126,42 +126,122 @@
 // // 10) Foydalanuvchidan telefon raqamini so‘rab, agar raqam
 // uzunligi 10 ta bo‘lsa va faqat raqamlardan iborat bo‘lsa
 // "Telefon raqami to‘g‘ri", aks holda "Telefon raqami noto‘g‘ri" deb chiqaring.
+// let raqam = parseInt(prompt("Raqamizi kiriting: "))
+// if (raqam.length === 10 && /^\d+$/.test(raqam)) {
+//   console.log("Telefon raqami to‘g‘ri");
+// } else {
+//   console.log("Telefon raqami noto‘g‘ri");
+// }
 
 // // 11)  Foydalanuvchidan so‘z yoki ibora so‘rab, agar u
 // palindrom bo‘lsa "Palindrom", aks holda "Palindrom emas" deb xabar bering.
 
+// let soz = prompt("So'zni kiriting");
+
+// if (soz.split("").reverse().join("") == soz) {
+//   console.log("Bu so'z palindrom");
+// } else {
+//   console.log("Paliindrom emas");
+// }
 // // 12)  Foydalanuvchidan matn so‘rab, agar birinchi va
 // oxirgi harflar bir xil bo‘lsa "Bir xil harf", aks holda
 // "Harflar farq qiladi" deb chiqaring.
+// let soz = prompt("So'zni kiriting");
+// if (soz[0] == soz.slice(-1)) {
+//   console.log("Bir xil harf");
+// } else {
+//   console.log("Harflar farq qiladi");
+// }
 
 // // 13) Foydalanuvchidan URL so‘rab, agar
 // http:// yoki https:// bilan boshlansa "URL to‘g‘ri",
 // aks holda "URL noto‘g‘ri" deb xabar bering.
+// let link = prompt("Linkni kiriting");
+
+// if (link.slice(0, 7) == "https://" || link.slice(0, 6) == "http://") {
+//   console.log("URL to‘g‘ri");
+// } else {
+//   console.log("URL noto‘g‘ri");
+// }
 
 // // 14) Foydalanuvchidan matn so‘rab, agar matn faqat
 // harflardan iborat bo‘lsa, harflarni alohida chiqarish,
 // aks holda "Noto‘g‘ri matn" deb chiqaring.
 
+// let matn = prompt("Matnni kiriting: ");
+// if (/^[a-zA-Z]+$/.test(matn)) {
+//   console.log(matn.split("").join("\n"));
+// } else {
+//   console.log("Noto‘g‘ri matn");
+// }
+
 // // 15) Foydalanuvchidan so‘z so‘rab, agar so‘z uzunligi
 // 5 ta belgidan oshsa "Katta so‘z", aks holda "Kichik so‘z"
 //  deb chiqarish.
-
+// let soz = prompt("Matnni kiriting: ");
+// if (soz.length() >= 5) {
+//   console.log("Katta so‘z");
+// } else {
+//   console.log("Kichik so‘z");
+// }
 // // 16) Foydalanuvchidan matn so‘rab, agar matnda katta
 //  harf bo‘lsa, birinchi katta harfni chiqarish, aks holda
 //  "Katta harf yo‘q" deb xabar bering.
 
+// let matn = "Codial";
+// if (/[A-Z]/.test(matn)) {
+//   console.log(matn.replace(/\d+/g, "").replace(/[a-z]+/g, ""));
+// } else {
+//   console.log("Katta harf yo‘q");
+// }
+
 // // 17) Foydalanuvchidan FIO (Familiya, Ism, Otasining ismi)
 //  so‘rab, agar har bir qism bosh harf bilan boshlansa
 //  "To‘g‘ri FIO", aks holda "Noto‘g‘ri FIO" deb chiqaring.
+// let fio = prompt("FIO kiriting: ");
 
+// let qismlar = fio.split(" ");
+
+// let togri = qismlar.every((qism) => /^[A-ZА-Я]/.test(qism));
+
+// if (togri) {
+//   console.log("To‘g‘ri FIO");
+// } else {
+//   console.log("Noto‘g‘ri FIO");
+// }
 // // 18) Foydalanuvchidan matn so‘rab, agar matn kamida ikki
 // so‘zdan iborat bo‘lsa, so‘zlarni teskari tartibda chiqarish,
 // aks holda "Kam so‘zli matn" deb xabar bering.
+// let matn = prompt("Matnni kiriting: ")
+// let sozlar = matn.trim().split(/\s+/);
 
+// if (sozlar.length >= 2) {
+//   console.log(sozlar.reverse().join(" "));
+// } else {
+//   console.log("Kam so‘zli matn");
+// }
 // // 19) Foydalanuvchidan URL so‘rab, agar URL parametrlarga
 //  ega bo‘lsa, parametrlarni ajratib chiqarish, aks holda
 //  "Parametrlar yo‘q" deb chiqaring.
+// let url = prompt("URL kiriting: ")
+// let urlObj = new URL(url);
+// let params = urlObj.searchParams;
 
+// // Parametrlar mavjudligini tekshirish
+// if (params.toString()) {
+//   // Parametrlarni chiqarish
+//   params.forEach((value, key) => {
+//     console.log(`${key}: ${value}`);
+//   });
+// } else {
+//   console.log("Parametrlar yo‘q");
+// }
 // // 20) Foydalanuvchidan matn so‘rab, agar matnda harflar va
 // raqamlar aralash bo‘lsa, "Miksa to‘g‘ri", aks holda
 // "Miksa noto‘g‘ri" deb xabar bering.
+// // let matn = prompt("Matnni kiriting: ")
+// if (/[a-zA-Z]/.test(matn) && /\d/.test(matn)) {
+//   console.log("Miksa to‘g‘ri");
+// } else {
+//   console.log("Miksa noto‘g‘ri");
+// }
