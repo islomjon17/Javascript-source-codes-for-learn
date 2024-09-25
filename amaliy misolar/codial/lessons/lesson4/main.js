5f34ded709b4a42ed396524e6745d36570190499
islomjon17 <turgunovislomjon107@gmail.com>
2024-09-25 17:00:57 +0500

    updated

---
+++ amaliy misolar/codial/lessons/lesson4/main.js
@@ -0,0 +1,14 @@
+function ttyt(a,b){
+    return a*b
+}
+
+console.log(ttyt(5,6));
+
+// function sqr(num) {
+//     return num * num;
+
+// }
+
+// let result = sqr(55);
+// console.log(sqr(55));
+// console.log(result);
