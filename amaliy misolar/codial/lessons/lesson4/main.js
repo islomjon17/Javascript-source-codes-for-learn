12a29ac75c094d279c02ff1c8b87525de0cd3681
islomjon17 <turgunovislomjon107@gmail.com>
2024-09-27 09:05:08 +0500

    updated

--- amaliy misolar/codial/lessons/lesson4/main.js
+++ amaliy misolar/codial/lessons/lesson4/main.js
@@ -1,8 +1,8 @@
-function ttyt(a,b){
-    return a*b
+function ttyt(a, b) {
+  return a * b;
 }
 
-console.log(ttyt(5,6));
+console.log(ttyt(5, 6));
 
 // function sqr(num) {
 //     return num * num;
@@ -12,3 +12,17 @@ console.log(ttyt(5,6));
 // let result = sqr(55);
 // console.log(sqr(55));
 // console.log(result);
+
+const func3 = (text1, character) => {
+  var arr = text1.split(" ");
+  console.log(arr);
+  var newArr = arr.filter((item) => {
+    return item.includes(x)
+  })
+  console.log(newArr);
+  
+  
+};
+
+var text1 = "lorem lorem onaons  osaiodnallkd dfslke ofslnf.";
+console.log(func3(text1, "lorem"));
